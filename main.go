b8aa2bbb40850a936d965b76586cfa586a8fb2bd
jacobh18 <jacobhuneke@gmail.com>
2026-03-06 10:21:58 -0800

    B: MiddlewareMetrics inc

--- main.go
+++ main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(http.StatusText(http.StatusOK)))
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
 	})
+}
+
+func main() {
+	apicfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	serveMux := http.NewServeMux()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", apicfg.middlewareMetricsInc(handler))
+	serveMux.HandleFunc("GET /healthz", handlerReadiness)
+	serveMux.HandleFunc("GET /metrics", apicfg.handlerNumReqs)
+	serveMux.HandleFunc("POST /reset", apicfg.handlerReqReset)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
+
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (a *apiConfig) handlerNumReqs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	str := fmt.Sprintf("Hits: %v", a.fileserverHits.Load())
+	w.Write([]byte(str))
+}
+
+func (a *apiConfig) handlerReqReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	a.fileserverHits.Store(0)
 }
